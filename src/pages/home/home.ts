483b951d109f7e1129c0c863f3e0bd5c5d0f6da9
Raihan <riger400@gmail.com>
2018-11-30 20:31:01 +0600

    workign on the dynamic css

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,6 +13,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 export class HomePage {
 
   @ViewChild(Slides) slides: Slides;
+  sliderOptions = { pager: true, autoHeight: true };
+  // myColour: string  = 'green';
 
   constructor(public navCtrl: NavController,private statusBar: StatusBar) {
     this.statusBar.overlaysWebView(true);
@@ -23,7 +25,12 @@ export class HomePage {
 
   slideChanged() {
     let currentIndex = this.slides.getActiveIndex();
+    // this.myColour = 'red';
     console.log('Current index is', currentIndex);
+  }
+
+  slideWillCHange() {
+    console.log('sdfsaldkjfhskldhfkshfksdjfh');
   }
 
 }

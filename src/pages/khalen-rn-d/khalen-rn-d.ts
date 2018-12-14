11cf79f5b0e3baa93eca20a613afb9fdac50a1bb
khaled301 <khaled301@users.noreply.github.com>
2018-12-14 21:17:07 +0600

    commit with home modification 01

--- src/pages/khalen-rn-d/khalen-rn-d.ts
+++ src/pages/khalen-rn-d/khalen-rn-d.ts
@@ -37,95 +37,175 @@ export class KhalenRnDPage{
   }
   buttonOnClink(el: string){
     if(el === 'boat' && this.current_angle == 0) {
-      $("#child-02, #child-03, #child-04, #child-05").show( "slow" );
+
+      $("#child").css({ width: '60px', height: '60px', top: '-40px', left: '672px' });
+      $("#child-02").css({ width: '55px', height: '55px', top: '-30px', left: '745px' });
+      $("#child-03").css({ width: '50px', height: '50px', top: '-15px', left: '811px' });
+      $("#child-04").css({ width: '55px', height: '55px', top: '-30px', left: '604px' });
+      $("#child-05").css({ width: '50px', height: '50px', top: '-15px', left: '543px' });
+
+      $("#child-02, #child-03, #child-04, #child-05").show( 300 );
       $("#child-02-duplicate, #child-03-duplicate, #child-04-duplicate, #child-05-duplicate").hide( );
+
     }
+
     else if(el === 'boat' && this.current_angle != 0) {
+
       this.current_angle = this.current_angle - this.current_angle;
+
       this.rotateClockWise(this.current_angle);
-      $("#child-02, #child-03, #child-04, #child-05").show( "slow" );
+
+      $("#child").css({ width: '60px', height: '60px', top: '-40px',  left: '672px' });
+      $("#child-02").css({ width: '55px', height: '55px', top: '-30px', left: '745px' });
+      $("#child-03").css({ width: '50px', height: '50px', top: '-15px', left: '811px' });
+      $("#child-04").css({ width: '55px', height: '55px', top: '-30px', left: '604px' });
+      $("#child-05").css({ width: '50px', height: '50px', top: '-15px', left: '543px' });
+
+      $("#child-02, #child-03, #child-04, #child-05").show( 300 );
       $("#child-02-duplicate, #child-03-duplicate, #child-04-duplicate, #child-05-duplicate").hide( );
     }
+
     else if (el === 'fish' && this.current_angle == 0) {
-      this.current_angle -= 45;
+
+      this.current_angle -= 6;
+
       this.rotateAntiClockWise(this.current_angle);
-      $("#child-05").hide( );
-      $("#child-05-duplicate").show( "slow" );
+
+      $("#child-02").css({ width: '60px', height: '60px', top: '-37px', left: '745px' });
+      $("#child").css({ width: '55px', height: '55px', top: '-35px', left: '677px' });
+      $("#child-03").css({ width: '55px', height: '55px', top: '-20px', left: '817px' });
+      $("#child-04").css({ width: '50px', height: '50px', top: '-26px', left: '614px' });
+      $("#child-05-duplicate").css({ width: '50px', height: '50px', top: '2px', left: '881px' });
+
+      $("#child-05, #child-02-duplicate, #child-03-duplicate, #child-04-duplicate").hide( );
+      $("#child-05-duplicate, #child, #child-02, #child-03, #child-04").show( 300 );
+
     }
+
     else if (el === 'fish' && this.current_angle != 0) {
-      console.log ("I'm here");
-      this.current_angle = -45;
+
+      this.current_angle = -6;
+
       this.rotateAntiClockWise(this.current_angle);
-      $("#child-05").hide( );
-      $("#child-05-duplicate").show( "slow" );
+
+      $("#child-02").css({ width: '60px', height: '60px', top: '-37px', left: '745px' });
+      $("#child").css({ width: '55px', height: '55px', top: '-35px', left: '677px' });
+      $("#child-03").css({ width: '55px', height: '55px', top: '-20px', left: '817px' });
+      $("#child-04").css({ width: '50px', height: '50px', top: '-26px', left: '614px' });
+      $("#child-05-duplicate").css({ width: '50px', height: '50px', top: '2px', left: '881px' });
+
+      $("#child-05, #child-02-duplicate, #child-03-duplicate, #child-04-duplicate").hide( );
+      $("#child-05-duplicate, #child, #child-02, #child-03, #child-04").show( 300 );
+
     }
+
     else if (el === 'cycling' && this.current_angle == 0) {
-      this.current_angle -= 90;
+
+      this.current_angle -= 11.5;
+
       this.rotateAntiClockWise(this.current_angle);
-      $("#child-04, #child-05, #child-02-duplicate, #child-03-duplicate").hide( );
-      $("#child-04-duplicate, #child-05-duplicate").show( "slow" );
+
+      $("#child-03").css({width: '60px', height: '60px', top: '-26px', left: '811px'});
+      $("#child").css({width: '50px', height: '50px', top: '-27px', left: '682px'});
+      $("#child-02").css({width: '55px', height: '55px', top: '-30px', left: '745px'});
+      $("#child-04-duplicate").css({width: '50px', height: '50px', top: '26px', left: '945px'});
+      $("#child-05-duplicate").css({width: '55px', height: '55px', top: '-2px', left: '881px'});
+
+      $("#child-04, #child-05, #child-02-duplicate, #child-03-duplicate").hide();
+      $("#child-04-duplicate, #child-05-duplicate, #child, #child-02, #child-03").show(300);
+
     }
+
     else if (el === 'cycling' && this.current_angle != 0) {
-      this.current_angle = -90;
+
+      this.current_angle = -11.5;
+
       this.rotateAntiClockWise(this.current_angle);
-      $("#child-04, #child-05, #child-02-duplicate, #child-03-duplicate").hide( );
-      $("#child-04-duplicate, #child-05-duplicate").show( "slow" );
+
+      $("#child-03").css({width: '60px', height: '60px', top: '-26px', left: '811px'});
+      $("#child").css({width: '50px', height: '50px', top: '-27px', left: '682px'});
+      $("#child-02").css({width: '55px', height: '55px', top: '-30px', left: '745px'});
+      $("#child-04-duplicate").css({width: '50px', height: '50px', top: '26px', left: '945px'});
+      $("#child-05-duplicate").css({width: '55px', height: '55px', top: '-2px', left: '881px'});
+
+      $("#child-04, #child-05, #child-02-duplicate, #child-03-duplicate").hide();
+      $("#child-04-duplicate, #child-05-duplicate, #child, #child-02, #child-03").show(300);
+
     }
+
     else if(el === 'climbing' && this.current_angle == 0) {
-      this.current_angle += 41;
+      this.current_angle += 7;
       this.rotateClockWise(this.current_angle);
-      $("#child-03, #child-02-duplicate, #child-04-duplicate, #child-05-duplicate").hide( );
-      $("#child-03-duplicate").show( "slow" );
+      // $("#child-03, #child-02-duplicate, #child-04-duplicate, #child-05-duplicate").hide( );
+      // $("#child-03-duplicate").show( "slow" );
     }
+
     else if(el === 'climbing' && this.current_angle != 0) {
-      this.current_angle = 41;
+      this.current_angle = 7;
       this.rotateClockWise(this.current_angle);
-      $("#child-03, #child-02-duplicate, #child-04-duplicate, #child-05-duplicate").hide( );
-      $("#child-03-duplicate").show( "slow" );
+      // $("#child-03, #child-02-duplicate, #child-04-duplicate, #child-05-duplicate").hide( );
+      // $("#child-03-duplicate").show( "slow" );
     }
+
     else if(el === 'tennis' && this.current_angle == 0) {
-      this.current_angle += 90;
+      this.current_angle += 13;
       this.rotateClockWise(this.current_angle);
-      $("#child-02, #child-03, #child-04-duplicate, #child-05-duplicate").hide( );
-      $("#child-02-duplicate, #child-03-duplicate").show( "slow" );
+      // $("#child-02, #child-03, #child-04-duplicate, #child-05-duplicate").hide( );
+      // $("#child-02-duplicate, #child-03-duplicate").show( "slow" );
     }
     else if(el === 'tennis' && this.current_angle != 0) {
-      this.current_angle = 90;
+      this.current_angle = 13;
       this.rotateClockWise(this.current_angle);
-      $("#child-02, #child-03, #child-04-duplicate, #child-05-duplicate").hide( );
-      $("#child-02-duplicate, #child-03-duplicate").show( "slow" );
+      // $("#child-02, #child-03, #child-04-duplicate, #child-05-duplicate").hide( );
+      // $("#child-02-duplicate, #child-03-duplicate").show( "slow" );
     }
     else if(el === 'cycling-duplicate' && this.current_angle == 0) {
-      this.current_angle += 135;
-      this.rotateClockWise(this.current_angle);
+      // this.current_angle += 135;
+      // this.rotateClockWise(this.current_angle);
+      this.current_angle -= 13;
+      this.rotateAntiClockWise(this.current_angle);
     }
     else if(el === 'cycling-duplicate' && this.current_angle != 0) {
-      this.current_angle = 135;
-      this.rotateClockWise(this.current_angle);
+      // this.current_angle = 135;
+      // this.rotateClockWise(this.current_angle);
+      this.current_angle = -13;
+      this.rotateAntiClockWise(this.current_angle);
     }
     else if(el === 'fish-duplicate' && this.current_angle == 0) {
-      this.current_angle += 180;
-      this.rotateClockWise(this.current_angle);
+      // this.current_angle += 180;
+      //       // this.rotateClockWise(this.current_angle);
+      this.current_angle -= 8;
+      this.rotateAntiClockWise(this.current_angle);
     }
     else if(el === 'fish-duplicate' && this.current_angle != 0) {
-      this.current_angle = 180;
-      this.rotateClockWise(this.current_angle);
+      // this.current_angle = 180;
+      // this.rotateClockWise(this.current_angle);
+      this.current_angle = -8;
+      this.rotateAntiClockWise(this.current_angle);
     }
     else if (el === 'climbing-duplicate' && this.current_angle == 0) {
-      this.current_angle -= 141;
-      this.rotateAntiClockWise(this.current_angle);
+      // this.current_angle -= 141;
+      // this.rotateAntiClockWise(this.current_angle);
+      this.current_angle += 7;
+      this.rotateClockWise(this.current_angle);
     }
     else if (el === 'climbing-duplicate' && this.current_angle != 0) {
-      this.current_angle = -141;
-      this.rotateAntiClockWise(this.current_angle);
+      // this.current_angle = -141;
+      // this.rotateAntiClockWise(this.current_angle);
+      this.current_angle = 7;
+      this.rotateClockWise(this.current_angle);
     }
     else if (el === 'tennis-duplicate' && this.current_angle == 0) {
-      this.current_angle -= 135;
-      this.rotateAntiClockWise(this.current_angle);
+      // this.current_angle -= 135;
+      // this.rotateAntiClockWise(this.current_angle);
+      this.current_angle += 13;
+      this.rotateClockWise(this.current_angle);
     }
     else if (el === 'tennis-duplicate' && this.current_angle != 0) {
-      this.current_angle = -135;
-      this.rotateAntiClockWise(this.current_angle);
+      // this.current_angle = -135;
+      // this.rotateAntiClockWise(this.current_angle);
+      this.current_angle = 13;
+      this.rotateClockWise(this.current_angle);
     }
     else if (el === 'boat-duplicate' && this.current_angle == 0) {
       this.current_angle -= 180;

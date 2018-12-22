9d32b2e373ffd939ec1922e09e82685a00d809ad
Raihan Zaman <raihan.zaman@amadeyr.org>
2018-12-22 14:34:20 +0600

    changing the class dynamically

--- src/pages/movies/movies.ts
+++ src/pages/movies/movies.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as $ from 'jquery';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MoviesPage {
 
+  firstImage: string = 'large-movies-div-img  first-image';
+  secondImage:string = 'large-movies-div-img second-image';
+  thirdImage:string = 'large-movies-div-img third-image';
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MoviesPage');
+  }
+
+  makeItMove(){
+    console.log('working');
+    this.firstImage = 'large-movies-div-img third-image';
+    this.secondImage = 'large-movies-div-img  first-image';
+    this.thirdImage = 'large-movies-div-img second-image';
   }
 
 }

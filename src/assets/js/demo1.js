0f0d16f5fc4257a679eec1cbe84d76a9aaa2c9df
khaled301 <khaled301@users.noreply.github.com>
2018-12-24 16:29:40 +0600

    commit with new slider settings beta-02 repositioning items on click

--- src/assets/js/demo1.js
+++ src/assets/js/demo1.js
@@ -8,7 +8,7 @@
     easing: 'easeInOutCubic', // easing function (used by anime.js)
     elasticity: 100, // elasticity factor (used by anime.js)
     rotate: true, // This indicates that items should be rotated properly to match the SVG path curve
-    activeSeparation: 65
+    activeSeparation: 68
   };
 
   // Initialize the slider using our SVG path, items, and options
--- src/components/custom-recreation-slider/custom-recreation-slider.ts
+++ src/components/custom-recreation-slider/custom-recreation-slider.ts
@@ -19,11 +19,7 @@ export class CustomRecreationSliderComponent {
   }
   itemOnClick(el:string) {
     console.log('hello =>' + el);
-      // $('.item_13').trigger('click');
-  this.itemHideAndSeek(el);
-  }
-
-  itemHideAndSeek(el:string) {
+    // $('.item_13').trigger('click');
     var item = '.item_' + el;
     var afterItem = '.a' + el;
     var beforeItem = '.b' + el;
@@ -31,6 +27,38 @@ export class CustomRecreationSliderComponent {
     var farBeforeItem = '.far-b' + el;
     var hideItems = '.hide-for-item' + el;
 
+    var itemPosition = '.item' + el;
+    var farAfterItemPosition = '.postion-' + 'far-a' + el;
+    var farBeforeItemPosition = '.postion-' + 'far-b' + el;
+    var afterItemPosition = '.postion-' + 'a' + el;
+    var beforeItemPosition = '.postion-' + 'b' + el;
+
+    this.itemHideAndSeek(item, afterItem, beforeItem, farAfterItem, farBeforeItem, hideItems);
+    this.adjacentItemsResizing(
+      item, afterItem, beforeItem, farAfterItem, farBeforeItem, itemPosition, farAfterItemPosition,
+      farBeforeItemPosition, afterItemPosition, beforeItemPosition );
+
+  }
+
+  adjacentItemsResizing( item:string, afterItem:string,
+                         beforeItem:string, farAfterItem:string, farBeforeItem:string, itemPosition:string,
+                         farAfterItemPosition:string, farBeforeItemPosition:string, afterItemPosition:string, beforeItemPosition:string )
+  {
+
+    $('' + itemPosition).css({ margin: '-10px 0 0 0'});
+    $('' + farAfterItemPosition).css({ margin: '-25px 0 0 0'});
+    $('' + farBeforeItemPosition).css({ margin: '-25px 0 0 0'});
+    $('' + afterItemPosition).css({ margin: '-10px 0 0 0'});
+    $('' + beforeItemPosition).css({ margin: '-10px 0 0 0'});
+
+    $('' + item).css({ transform: 'scale(1.2)'});
+    $('' + farAfterItem).css({ transform: 'scale(0.9)' });
+    $('' + farBeforeItem).css({ transform: 'scale(0.9)'});
+    $('' + afterItem).css({ transform: 'scale(1.05)' });
+    $('' + beforeItem).css({ transform: 'scale(1.05)' });
+  }
+
+  itemHideAndSeek(item:string, afterItem:string, beforeItem:string, farAfterItem:string, farBeforeItem:string, hideItems:string) {
     $('' + hideItems).css({ opacity: '0', transition: '0.35s' });
     setTimeout(function(){
       $('' + hideItems).css({ display: 'none'});
@@ -38,8 +66,8 @@ export class CustomRecreationSliderComponent {
 
     $('' + item).css({ display: 'inherit'});
     setTimeout(function(){
-      $('' + item).css({ opacity: '1', transition: '0.39s' });
-    }, 390);
+      $('' + item).css({ opacity: '1', transition: '0.4s' });
+    }, 400);
 
     $('' + afterItem).css({ display: 'inherit'});
     $('' + beforeItem).css({ display: 'inherit'});

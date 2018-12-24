a7c8113238e8b148d6f97878c608322d4579577b
khaled301 <khaled301@users.noreply.github.com>
2018-12-24 14:14:16 +0600

    commit with new slider settings beta-01 radial movement fix

--- src/components/custom-recreation-slider/custom-recreation-slider.ts
+++ src/components/custom-recreation-slider/custom-recreation-slider.ts
@@ -17,9 +17,43 @@ export class CustomRecreationSliderComponent {
   constructor() {
 
   }
-  itemOnClick() {
-    console.log('hello');
-      $('.item_13').trigger('click');
+  itemOnClick(el:string) {
+    console.log('hello =>' + el);
+      // $('.item_13').trigger('click');
+  this.itemHideAndSeek(el);
+  }
+
+  itemHideAndSeek(el:string) {
+    var item = '.item_' + el;
+    var afterItem = '.a' + el;
+    var beforeItem = '.b' + el;
+    var farAfterItem = '.far-a' + el;
+    var farBeforeItem = '.far-b' + el;
+    var hideItems = '.hide-for-item' + el;
+
+    $('' + hideItems).css({ opacity: '0', transition: '0.35s' });
+    setTimeout(function(){
+      $('' + hideItems).css({ display: 'none'});
+    }, 350);
+
+    $('' + item).css({ display: 'inherit'});
+    setTimeout(function(){
+      $('' + item).css({ opacity: '1', transition: '0.39s' });
+    }, 390);
+
+    $('' + afterItem).css({ display: 'inherit'});
+    $('' + beforeItem).css({ display: 'inherit'});
+    setTimeout(function(){
+      $('' + afterItem).css({ opacity: '1', transition: '0.40s' });
+      $('' + beforeItem).css({ opacity: '1', transition: '0.40s' });
+    }, 400);
+
+    $('' + farAfterItem).css({ display: 'inherit'});
+    $('' + farBeforeItem ).css({ display: 'inherit'});
+    setTimeout(function(){
+      $('' + farAfterItem).css({ opacity: '1', transition: '0.5s' });
+      $('' + farBeforeItem ).css({ opacity: '1', transition: '0.5s' });
+    }, 500);
 
   }
 }

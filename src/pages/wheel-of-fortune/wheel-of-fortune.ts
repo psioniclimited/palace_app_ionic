6b6491de66812c7a55ed415c0e2c602a47d5c2fc
Raihan Zaman <raihan.zaman@amadeyr.org>
2019-01-07 12:55:25 +0600

    need more rotation

--- src/pages/wheel-of-fortune/wheel-of-fortune.ts
+++ src/pages/wheel-of-fortune/wheel-of-fortune.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+declare var TweenMax: any;
+declare var Power1: any;
+declare var Power4: any;
 @IonicPage()
 @Component({
   selector: 'page-wheel-of-fortune',
@@ -19,10 +21,32 @@ export class WheelOfFortunePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WheelOfFortunePage');
+    TweenMax.from(".logo", .5, {
+      opacity: 0,
+      y: -72,
+      ease:  Power1.easeOut
+    });
   }
+
+
 
   spin(){
     console.log('testing');
+    this.animateWheel(40);
+  }
+
+  animateWheel(duration){
+    let randomNum = Math.floor((Math.random() * 10));
+    this.rotation += (30 * duration) + (30 * randomNum);
+    TweenMax.to('.wheel', 20, {
+      ease: Power4.easeOut,
+      rotation: this.rotation,
+      onComplete: this.resetWheel()
+    });
+  }
+
+  resetWheel(){
+    this.slowingDown = false;
   }
 
 }

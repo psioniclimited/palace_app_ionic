cd6ec3ee774f6ac84403475b615e949f3d1f49eb
Raihan Zaman <raihan.zaman@amadeyr.org>
2019-01-13 11:40:27 +0600

    working on rotating the wheel

--- src/pages/wheel-of-fortune/wheel-of-fortune.ts
+++ src/pages/wheel-of-fortune/wheel-of-fortune.ts
@@ -16,7 +16,18 @@ export class WheelOfFortunePage {
   }
 
   spin(){
+
+    let prize = (Math.floor(Math.random() * 6)) + 1;
+    // Spin to the angle of the segment based on the random number
+    let segmentAngle = ((prize - 1) * -60);
+    // Add on 3 full spins
+    let angle = segmentAngle + 1080;
     console.log('tesitng');
+    let wheel = document.getElementById('backdrop');
+    wheel.classList.add("animate");
+    wheel.style.webkitTransform = 'rotate('+angle+'deg)';
+    wheel.style.transform = 'rotate('+angle+'deg)';
+
   }
 
 }
